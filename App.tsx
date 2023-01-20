346c7d61b8fadb59d0f560055790bf645977966d
josealejandro2928 <37028825+josealejandro2928@users.noreply.github.com>
2023-01-20 17:17:09 +0100

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import ReactImagePickerEditor, {
+  ImagePickerConf,
+} from 'react-image-picker-editor';
+
+import 'react-image-picker-editor/dist/index.css';
+
+const App = () => {
+  const config: ImagePickerConf = {
+    borderRadius: '8px',
+    language: 'en',
+    width: '330px',
+    height: '250px',
+    objectFit: 'contain',
+    // aspectRatio: 4 / 3,
+    compressInitial: null,
+  };
+
+  const [imageSrc, setImageSrc] = useState<string | null | undefined>('');
+  const initialImage = '';
+
+  return (
+    <div className="container">
+      <ReactImagePickerEditor
+        config={config}
+        imageSrcProp={initialImage}
+        imageChanged={(newDataUri: any) => {
+          setImageSrc(newDataUri);
+        }}
+      />
+      <br /> <br />
+      <hr />
+      <br />
+      <p>setImageSrc:</p>
+      {imageSrc && (
+        <img
+          src={imageSrc}
+          alt="example"
+          style={{
+            maxHeight: '900px',
+            maxWidth: '100%',
+            objectFit: 'contain',
+            background: 'black',
+          }}
+        />
+      )}
+      {!imageSrc && (
+        <h2 style={{ textAlign: 'center' }}>No image loaded yet</h2>
+      )}
+    </div>
+  );
+};
+
+export default App;
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-image-picker-editor-example-usage
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-image-picker-editor-example-usage)
